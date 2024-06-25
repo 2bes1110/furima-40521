6e500d2ff5da07b84ae81628e7b37959e4e712d9
2bes1110 <165120772+2bes1110@users.noreply.github.com>
2024-06-25 11:32:46 +0900

    ユーザ管理機能実装テスト修正#1

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,21 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname, presence: true
-         validates :last_name, presence: true
-         validates :first_name, presence: true
-         validates :last_furigana, presence: true
-         validates :first_furigana, presence: true
+
          validates :birthday, presence: true
+
+         with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+          validates :last_name
+          validates :first_name
+        end
+        
+        with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナのみで入力して下さい' } do
+          validates :last_furigana
+          validates :first_furigana
+        end
+        
+        PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+        validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+#          VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+# validates :password, format: { with: VALID_PASSWORD_REGEX }
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.email}
+    email                 { Faker::Internet.email }
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
-    last_name             {Faker::Name.initials(number: 2)}
-    first_name            {Faker::Name.initials(number: 2)}
-    last_furigana         {Faker::Name.initials(number: 2)}
-    first_furigana        {Faker::Name.initials(number: 2)}
+    last_name             { '山田' }
+    first_name            { '太朗' }
+    last_furigana         { 'ヤマダ' }
+    first_furigana        { 'タロウ' }
     birthday              {Faker::Date.birthday(min_age: 18, max_age: 65)}
   end
 end

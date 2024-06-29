ef348ab590e72a8d827dc2abbebd9f3bd0c0a741
2bes1110 <165120772+2bes1110@users.noreply.github.com>
2024-06-29 17:00:46 +0900

    商品出品機能修正#1

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,15 +1,30 @@
-window.addEventListener('turbo:load', () => {
+// window.addEventListener('turbo:load', () => {
+// });
+
+
+// const priceInput = document.getElementById("item-price");
+// const addTaxDom = document.getElementById("add-tax-price");
+// const profit = document.getElementById("profit");
+// priceInput.addEventListener("input", () => {
+//   const inputValue = priceInput.value;
+
+//   addTaxDom.innerHTML = Math.ceil(inputValue * 0.1);
+//   addProfit.innerHTML = Math.floor(inputValue - addTaxDom);
+// })
+
+
+
+window.addEventListener("DOMContentLoaded", () => {
+  if ( document.getElementById('item-price')!= null) {
+    // 読み込まれたページに'item-price'というid名の要素があれば発火
+    const priceInput = document.getElementById("item-price");
+    const addTaxDom = document.getElementById("add-tax-price");
+    const profitDom = document.getElementById("profit");
+
+    priceInput.addEventListener("input", () => {
+      const inputValue = document.getElementById("item-price").value;
+        addTaxDom.innerHTML = Math.floor(inputValue * 0.1).toLocaleString(); 
+        profitDom.innerHTML = Math.ceil(inputValue * 0.9).toLocaleString();
+    })
+  }
 });
-
-// 金額を入力した数値をpriceInputという変数に格納する
-const priceInput = document.getElementById("item-price");
-const addTaxDom = document.getElementById("add-tax-price");
-const profit = document.getElementById("profit");
-
-
-priceInput.addEventListener("input", () => {
-  const inputValue = document.getElementById("item-price").value;
-  
-    addTaxDom.innerHTML = Math.floor(inputValue * 0.1).toLocaleString(); 
-    profit.innerHTML = Math.ceil(inputValue * 0.9).toLocaleString();
-})
